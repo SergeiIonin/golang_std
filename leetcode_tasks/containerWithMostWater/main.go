5b240a6b6973ffda97cdbdb7c56fad841f0192d0
SergeiIonin <sai1042@yandex.ru>
2023-05-31 13:02:22 -0400

    add reading of large input from file

--- leetcode_tasks/containerWithMostWater/main.go
+++ leetcode_tasks/containerWithMostWater/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
@@ -9,17 +14,47 @@ func main() {
 	input1 := []int{1, 1}                      // 1
 	input2 := []int{1, 5, 16, 16, 5, 3}        // 16
 	input3 := []int{1, 5, 16, 14, 5, 3}        // 15
+	input4 := readInput()                      // 4913370
 
 	a0 := maxArea(input0)
 	a1 := maxArea(input1)
 	a2 := maxArea(input2)
 	a3 := maxArea(input3)
+	a4 := maxArea(input4)
 
 	fmt.Println("a0 = ", a0)
 	fmt.Println("a1 = ", a1)
 	fmt.Println("a2 = ", a2)
 	fmt.Println("a3 = ", a3)
+	fmt.Println("a4 = ", a4)
 
+}
+
+func readInput() []int {
+	f, err := os.Open("resources/input")
+	if err != nil {
+		fmt.Println("error = ", err.Error())
+	}
+	r := bufio.NewReader(f)
+	ints := make([]int, 0, 10000)
+
+	for {
+		b, err := r.ReadBytes(byte(','))
+		if err != nil {
+			fmt.Println("end of file:", err.Error())
+			break
+		}
+		bNew := bytes.TrimSuffix(b, []byte(","))
+
+		s := string(bNew)
+		int, err := strconv.Atoi(s)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		ints = append(ints, int)
+	}
+	return ints
 }
 
 // https://leetcode.com/problems/container-with-most-water/description/
@@ -35,77 +70,69 @@ func main() {
 func maxArea(height []int) int {
 
 	size := len(height)
-	visitedIndexes := make([]int, 0, size)
 	s := 0
 
+	var heightWithIndexes HeightIndexed
+	heightWithIndexes = make([]ElemWithIndex, size)
+
+	for i, v := range height {
+		heightWithIndexes[i] = ElemWithIndex{elem: v, index: i}
+	}
+
+	sort.Sort(heightWithIndexes) // asdending order
+	lenLeft := 0
+	lenRight := 0
+	var elemWithIndex ElemWithIndex
+
 	for i := 0; i < size; i++ {
-		max, index := maxWithIndex(height, &visitedIndexes)
-		fmt.Println("max = ", max)
-		fmt.Println("visitedIndexes = ", visitedIndexes)
-		if max*size < s {
-			continue
+		elemWithIndex = heightWithIndexes[i]
+		el := elemWithIndex.elem
+		ind := elemWithIndex.index
+
+		lenLeft = elemWithIndex.index
+		lenRight = size - lenLeft - 1
+
+		if lenLeft*el < s && lenRight*el < s {
+			break
 		}
-		s = getS(height, max, index, s, size)
+
+		sCur := 0
+
+		for j, cur := range height {
+			if cur < el || ind == j {
+				continue
+			}
+			sCur = abs(ind-j) * el
+			if sCur > s {
+				s = sCur
+			}
+		}
 	}
 
 	return s
+
 }
 
-func indexVisited(ind int, indexes []int) bool {
-	for i := 0; i < len(indexes); i++ {
-		if indexes[i] == ind {
-			return true
-		}
-	}
-	return false
+type ElemWithIndex struct {
+	elem  int
+	index int
 }
 
-// current max among the elems which haven't been visited yet
-// it works but it's not enough fast
-func maxWithIndex(h []int, visitedIndexes *[]int) (max int, index int) {
-	index = 0
-	max = h[index]
-	size := len(h)
-	if size == 1 {
-		return
-	}
-	for i := 0; i < size; i++ {
-		if indexVisited(i, *visitedIndexes) {
-			continue
-		}
-		if max < h[i] {
-			max = h[i]
-			index = i
-		}
-	}
-	*visitedIndexes = append(*visitedIndexes, index)
-	return
+type HeightIndexed []ElemWithIndex
+
+func (hi HeightIndexed) Len() int {
+	return len(hi)
+}
+func (hi HeightIndexed) Swap(i, j int) {
+	hi[i], hi[j] = hi[j], hi[i]
+}
+func (hi HeightIndexed) Less(i, j int) bool {
+	return hi[i].elem < hi[j].elem
 }
 
-func minOfTwo(left int, right int) int {
-	if left < right {
-		return left
-	} else {
-		return right
+func abs(v int) int {
+	if v < 0 {
+		return -v
 	}
-}
-
-func getS(h []int, max int, index int, sMax int, size int) (s int) {
-	sTemp := 0
-	s = sMax
-	fmt.Println("sBase = ", s)
-	for i, v := range h {
-		if index == i {
-			continue
-		}
-		sTemp = (index - i) * minOfTwo(max, v)
-		if sTemp < 0 {
-			sTemp = sTemp * (-1)
-		}
-		if sTemp > s {
-			s = sTemp
-		}
-	}
-	fmt.Println("s = ", s)
-	return s
+	return v
 }
