9caa73a9951e433cdb17b67b3d855d6f38ecfeb8
SergeiIonin <sai1042@yandex.ru>
2023-06-06 19:54:18 -0400

    rfctr mergeSortedArrays

--- leetcode_tasks/mergeSortedArrays/main.go
+++ leetcode_tasks/mergeSortedArrays/main.go
@@ -8,27 +8,60 @@ func main() {
 	nums2 := []int{2, 5, 6}
 	n := 3
 
-	r := merge(nums1, m, nums2, n)
+	// nums1 := []int{2, 0}
+	// m := 1
+	// nums2 := []int{1}
+	// n := 1
 
-	fmt.Println(r)
+	merge(nums1, m, nums2, n)
+
+	fmt.Println(nums1)
 }
 
-func merge(nums1 []int, m int, nums2 []int, n int) []int {
-	if m == 0 {
-		return nums2
-	}
-	if n == 0 {
-		return nums1[:m]
-	}
-	{
-		head1 := nums1[0]
-		head2 := nums2[0]
-		if head1 < head2 {
-			nums1 = append([]int{head1}, merge(nums1[1:], m-1, nums2, n)...)
-			return nums1
-		} else {
-			nums2 = append([]int{head2}, merge(nums1, m, nums2[1:], n-1)...)
-			return nums2
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if len(nums1) > 0 {
+		buf := append([]int{}, nums1[:m]...)
+		k := 0 // for buf1
+		j := 0 // for nums2
+		for i := 0; i < len(nums1); i++ {
+			if j < n {
+				if k != m {
+					head1 := buf[k]
+					head2 := nums2[j]
+					if head1 < head2 {
+						nums1[i] = head1
+						k++
+					} else {
+						nums1[i] = head2
+						j++
+					}
+				} else {
+					nums1[i] = nums2[j]
+					j++
+				}
+			} else {
+				nums1[i] = buf[k]
+				k++
+			}
 		}
 	}
 }
+
+// NB it may be more convenient to start from the end of the array (solution from leetcode):
+// func merge(nums1 []int, m int, nums2 []int, n int) {
+// 	i, j, k := m-1, n-1, m+n-1
+// 	for i >= 0 && j >= 0 {
+// 		if nums1[i] > nums2[j] {
+// 			nums1[k] = nums1[i]
+// 			i--
+// 		} else {
+// 			nums1[k] = nums2[j]
+// 			j--
+// 		}
+// 		k--
+// 	}
+// 	for j >= 0 {
+// 		nums1[j] = nums2[j]
+// 		j--
+// 	}
+// }
