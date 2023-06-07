0c3a084b261aa01f5c92d1b5ca37bf9cfc32b534
SergeiIonin <sai1042@yandex.ru>
2023-06-06 20:26:53 -0400

    rfctr Pascal triangle

--- leetcode_tasks/PascalTriangle/main.go
+++ leetcode_tasks/PascalTriangle/main.go
@@ -7,27 +7,22 @@ func main() {
 	fmt.Println(res)
 }
 
+// this solution beats 100% by runtime and 41.94% by memory
 func generate(numRows int) [][]int {
 	accum := make([][]int, numRows)
 	accum[0] = []int{1}
-	return rec(1, accum)
-}
-
-func rec(lvl int, accum [][]int) [][]int {
-	if lvl == len(accum) {
-		return accum
-	} else {
-		prev := accum[lvl-1]
+	for i := 1; i < len(accum); i++ {
+		prev := accum[i-1]
 		lenNext := len(prev) + 1
 		next := make([]int, lenNext)
-		for i := 0; i < lenNext; i++ {
-			if i == 0 || i == lenNext-1 {
-				next[i] = 1
+		for j := 0; j < lenNext; j++ {
+			if j == 0 || j == lenNext-1 {
+				next[j] = 1
 			} else {
-				next[i] = prev[i-1] + prev[i]
+				next[j] = prev[j-1] + prev[j]
 			}
 		}
-		accum[lvl] = next
-		return rec(lvl+1, accum)
+		accum[i] = next
 	}
+	return accum
 }
