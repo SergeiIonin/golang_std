2d9fdd8f90efff4e822f93065bb33d1d1e8ee01b
SergeiIonin <sai1042@yandex.ru>
2023-06-19 15:45:30 -0400

    add comments to bestTimeBuySellStocks

--- leetcode_tasks/bestTimeBuySellStocks/main.go
+++ leetcode_tasks/bestTimeBuySellStocks/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
 func main() {
 	p0 := []int{7, 1, 5, 3, 6, 4}
 	p1 := []int{7, 6, 4, 3, 1}
