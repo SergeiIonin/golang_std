dfd0285fcdd547b8bf4ee621d46d8c5d9a1b8453
SergeiIonin <sai1042@yandex.ru>
2024-09-24 11:39:32 -0300

    [leetcode/easy/two_pointers] merge old and new solutions

--- leetcode_tasks/easy/two_pointers/move_zeroes/main.go
+++ leetcode_tasks/easy/two_pointers/move_zeroes/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	in := []int{0,1,0,3,12}
+	in := []int{0, 1, 0, 3, 12}
 	moveZeroes(in)
 	fmt.Printf("res = %v\n", in)
 }
 
-/*https://leetcode.com/problems/move-zeroes/
+/*
+https://leetcode.com/problems/move-zeroes/
 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 
@@ -21,28 +22,23 @@ Example 2:
 
 Input: nums = [0]
 Output: [0]
-    
+
 Constraints:
-    1 <= nums.length <= 10^4
-    -2^31 <= nums[i] <= 2^31 - 1
+
+	1 <= nums.length <= 10^4
+	-2^31 <= nums[i] <= 2^31 - 1
 */
-func moveZeroes(nums []int)  {
-    if len(nums) <= 1 {
-        return
-    } else {
-        slow := 0
-        fast := 1
-        for ; fast < len(nums); {
-            if nums[fast] != 0 {
-                if nums[slow] == 0 {
-                    nums[slow] = nums[fast]
-                    nums[fast] = 0
-                }
-            }
-            if nums[slow] != 0 {
-                slow++
-            }
-            fast++
-        }
-    }
+func moveZeroes(nums []int) {
+	i := 0
+	j := 1
+	for j < len(nums) {
+		if nums[i] == 0 && nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		if nums[i] != 0 {
+			i++
+		}
+		j++
+	}
 }
