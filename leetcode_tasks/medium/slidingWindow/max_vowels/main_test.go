14f7e0614ec5578c656d37b7fde965204461b68f
SergeiIonin <sai1042@yandex.ru>
2024-09-25 23:57:14 -0300

    [leetcode/medium/slidingWindow] add maxVowels test

---
+++ leetcode_tasks/medium/slidingWindow/max_vowels/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMaxVowels(t *testing.T) {
+	tests := []struct {
+		name     string
+		str      string
+		k 		 int
+		expected int
+	}{
+		{"Test 1", "abciiidef", 3, 3},
+		{"Test 2", "aeiou", 2, 2},
+		{"Test 3", "leetcode", 3, 2},
+		{"Test 4", "twfsikrausdeuelcgiupwktszz", 5, 3},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := maxVowels(tt.str, tt.k)
+			if res != tt.expected {
+				t.Errorf("got %v, want %v", res, tt.expected)
+			}
+		})
+	}
+}
