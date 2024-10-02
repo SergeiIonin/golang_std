b63ab61d34d39e7ed3b8dacf7f10bedbbc3561c2
SergeiIonin <sai1042@yandex.ru>
2024-10-01 23:59:23 -0300

    [leetcode/easy/prefix_sum] add larg_alt test

---
+++ leetcode_tasks/easy/prefix_sum/largest_altitude/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestLargestAltitude(t *testing.T) {
+    tests := []struct {
+        name     string
+        in      []int
+        expected int
+    }{
+        {"Test 1", []int{-5,1,5,0,-7}, 1},
+        {"Test 2", []int{-4,-3,-2,-1,4,3,2}, 0},
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            result := largestAltitude(tt.in)
+            if result != tt.expected {
+                t.Errorf("got %v, want %v", result, tt.expected)
+            }
+        })
+    }
+}
